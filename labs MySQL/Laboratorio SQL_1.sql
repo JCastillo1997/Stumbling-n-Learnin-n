b84c984d4c0464e0896ee34e4edb543ea3114404
unknown <castillof1997@gmail.com>
2024-02-05 22:15:04 +0100

    sql work

---
+++ labs MySQL/Laboratorio SQL_1.sql
@@ -0,0 +1,18 @@
+USE LAB_2;
+insert into vehicle (`idCar`,`manufacturer`,`model`,`year of fabrication`,`Car colour`)
+values ('1','Volkswagen','Tiguan','2019','Blue'),
+('2','Peugeot',	'Rifter','2019','Red'),
+('3','Ford','Fusion','2018','White'),
+('4','Toyota','RAV4','2018','Silver'),
+('5','Volvo','V60','2019','Gray'),
+('6','Volvo','V60 Cross Country','2019','Gray');
+ 
+ insert into salesperson ('id','staff_id','name','Store')
+ values ('1','00001','Petey Cruiser','Madrid'),
+('2','00002',	'Anna Sthesia','Barcelona'),
+('3','00003','Paul Molive','Berlin'),
+('4','00004','Gail Forcewind','Paris'),
+('5','00005','Paige Turner'	,'Mimia'),
+('6','00006','Bob Frapples'	,'Mexico City'),
+('7','00007','Walter Melon','Amsterdam'),
+('8','00008','Shonda Leer',	'São Paulo')
