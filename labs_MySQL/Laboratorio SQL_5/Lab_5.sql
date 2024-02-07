ab781c9ad0e9e1c54dd86df7e18e3483f3cce970
unknown <castillof1997@gmail.com>
2024-02-07 18:02:07 +0100

    error with git

--- labs_MySQL/Laboratorio SQL_5/Lab_5.sql
+++ labs_MySQL/Laboratorio SQL_5/Lab_5.sql
@@ -9,3 +9,15 @@ where title  = 'Hunchback Impossible'
 Me encontraba incapaz de hacer el ejercicio metiendo una subquery, tras obtener el resultado
 gracias a chat gpt y ver que es menos claro que usar un join normal adjunto la solución como comentario.
 */
+
+-- List all films whose length is longer than the average length of all the films in the Sakila database.
+select title from film where film.length > (select avg(film.length) from film)
+
+-- Use a subquery to display all actors who appear in the film "Alone Trip".
+
+select a.first_name,a.last_name,f.title from actor as a
+left join film_actor as fa
+on a.actor_id = fa.actor_id
+left join film as f
+on fa.film_id = f.film_id
+where f.title ='Hunchback Impossible'
