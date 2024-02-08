ce4e923a468ffae2a5d86c076a12a720414da825
unknown <castillof1997@gmail.com>
2024-02-08 15:24:46 +0100

    Final exercise

--- labs_MySQL/Laboratorio SQL_6/Lab_6.sql
+++ labs_MySQL/Laboratorio SQL_6/Lab_6.sql
@@ -27,8 +27,20 @@ order by total_spent
 Step 3: Create a CTE and the Customer Summary Report
 Create a CTE that joins the rental summary View with the customer payment summary Temporary Table created in Step 2.
 The CTE should include the customer's name, email address, rental count, and total amount paid.
+*/
+with CTE_table as
+(select e.customer_id,e.nombre,email,`times rented`,total_spent from exercise_1 as e
+join gastos_1 as g
+on e.customer_id =g.customer_id )
+select * from CTE_table
 
+/*
 Next, using the CTE, create the query to generate the final customer summary report,
 which should include: customer name, email, rental_count, total_paid and average_payment_per_rental,
 this last column is a derived column from total_paid and rental_count.
 */
+with CTE_table as
+(select e.customer_id,e.nombre,email,round(total_spent/`times rented`,2) as beneficios_tiempo from exercise_1 as e
+join gastos_1 as g
+on e.customer_id =g.customer_id )
+select * from CTE_table
